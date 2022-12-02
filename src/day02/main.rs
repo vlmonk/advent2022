4deda1b5643cb9207fe5151081ec604e0cfaae33
Eugene Korbut <me@mvl.ru>
2022-12-02 18:42:03 +0600

    Day 02

--- src/day02/main.rs
+++ src/day02/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Move {
     Rock,
     Paper,
@@ -8,31 +8,12 @@ enum Move {
 }
 
 impl Move {
-    pub fn left(input: &str) -> Result<Self> {
-        let left = input
-            .chars()
-            .nth(0)
-            .ok_or_else(|| anyhow!("Missing left char"))?;
-
-        match left {
-            'A' => Ok(Move::Rock),
-            'B' => Ok(Move::Paper),
-            'C' => Ok(Move::Scissors),
-            _ => Err(anyhow!("Invalid char: {}", left)),
-        }
-    }
-
-    pub fn right(input: &str) -> Result<Self> {
-        let right = input
-            .chars()
-            .nth(2)
-            .ok_or_else(|| anyhow!("Missing right char"))?;
-
-        match right {
-            'X' => Ok(Move::Rock),
-            'Y' => Ok(Move::Paper),
-            'Z' => Ok(Move::Scissors),
-            _ => Err(anyhow!("Invalid char: {}", right)),
+    pub fn parse(input: char) -> Result<Self> {
+        match input {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(anyhow!("Invalid char: {}", input)),
         }
     }
 
@@ -45,6 +26,7 @@ impl Move {
     }
 }
 
+#[derive(Debug)]
 enum Outcome {
     Lost,
     Draw,
@@ -59,18 +41,27 @@ impl Outcome {
             Self::Won => 6,
         }
     }
+
+    pub fn parse(input: char) -> Result<Self> {
+        match input {
+            'X' => Ok(Outcome::Lost),
+            'Y' => Ok(Outcome::Draw),
+            'Z' => Ok(Outcome::Won),
+            _ => Err(anyhow!("Invalid char for outcome: {}", input)),
+        }
+    }
 }
 
 #[derive(Debug)]
-struct Round {
+struct ScoreRound {
     left: Move,
     right: Move,
 }
 
-impl Round {
+impl ScoreRound {
     pub fn parse(input: &str) -> Result<Self> {
-        let left = Move::left(input)?;
-        let right = Move::right(input)?;
+        let left = nth_char(input, 0).and_then(Move::parse)?;
+        let right = nth_char(input, 2).and_then(Move::parse)?;
 
         Ok(Self { left, right })
     }
@@ -93,16 +84,73 @@ impl Round {
 }
 
 #[derive(Debug)]
-
-struct Game {
-    rounds: Vec<Round>,
+struct GuessRound {
+    left: Move,
+    outcome: Outcome,
 }
 
-impl Game {
+fn nth_char(input: &str, n: usize) -> Result<char> {
+    input
+        .chars()
+        .nth(n)
+        .ok_or_else(|| anyhow!("Char {} not found for input '{}'", n, input))
+}
+
+impl GuessRound {
+    pub fn parse(input: &str) -> Result<Self> {
+        let left = nth_char(input, 0).and_then(Move::parse)?;
+        let outcome = nth_char(input, 2).and_then(Outcome::parse)?;
+
+        Ok(Self { left, outcome })
+    }
+
+    pub fn score(&self) -> i32 {
+        self.right().score() + self.outcome.score()
+    }
+
+    fn right(&self) -> Move {
+        match (&self.left, &self.outcome) {
+            (Move::Rock, Outcome::Lost) => Move::Scissors,
+            (Move::Rock, Outcome::Won) => Move::Paper,
+            (Move::Paper, Outcome::Lost) => Move::Rock,
+            (Move::Paper, Outcome::Won) => Move::Scissors,
+            (Move::Scissors, Outcome::Lost) => Move::Paper,
+            (Move::Scissors, Outcome::Won) => Move::Rock,
+            _ => self.left.clone(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ScoreGame {
+    rounds: Vec<ScoreRound>,
+}
+
+impl ScoreGame {
     pub fn parse(input: &str) -> Result<Self> {
         let rounds = input
             .lines()
-            .map(|line| Round::parse(line))
+            .map(|line| ScoreRound::parse(line))
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(Self { rounds })
+    }
+
+    pub fn score(&self) -> i32 {
+        self.rounds.iter().map(|round| round.score()).sum()
+    }
+}
+
+#[derive(Debug)]
+struct GuessGame {
+    rounds: Vec<GuessRound>,
+}
+
+impl GuessGame {
+    pub fn parse(input: &str) -> Result<Self> {
+        let rounds = input
+            .lines()
+            .map(|line| GuessRound::parse(line))
             .collect::<Result<Vec<_>>>()?;
 
         Ok(Self { rounds })
@@ -119,8 +167,12 @@ fn main() -> Result<()> {
         .ok_or_else(|| anyhow!("missing input filename"))?;
 
     let data = std::fs::read_to_string(&filename)?;
-    let game = Game::parse(&data)?;
-    println!("Score: {}", game.score());
+
+    let game = ScoreGame::parse(&data)?;
+    println!("Round A: {}", game.score());
+
+    let game = GuessGame::parse(&data)?;
+    println!("Round B: {}", game.score());
 
     Ok(())
 }
